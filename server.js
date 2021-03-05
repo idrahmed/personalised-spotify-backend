cec0fb09c418cf38ec8869e4424b334fde640566
idrah07 <idrisahmed987@gmail.com>
2021-03-05 21:07:18 +1100

    fixed up callback

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import SpotifyWebApi from "spotify-web-api-node"
+import SpotifyWebApi from "spotify-web-api-node";
 import express from "express";
 import cors from "cors";
-import recommendationsRouter from './routes/recommendationRoutes.js'
-import topTracksRouter from './routes/topTracksRoutes.js'
-import topArtistsRouter from './routes/topArtistsRoutes.js'
+import recommendationsRouter from "./routes/recommendationRoutes.js";
+import topTracksRouter from "./routes/topTracksRoutes.js";
+import topArtistsRouter from "./routes/topArtistsRoutes.js";
 
 const scopes = [
   "user-read-recently-played",
@@ -13,16 +13,15 @@ const scopes = [
   "user-follow-modify",
   "user-library-modify",
   "user-library-read",
-  "playlist-read-private"
-
+  "playlist-read-private",
 ];
 
 // credentials are optional
 export const spotifyApi = new SpotifyWebApi({
   clientId: "ec4be58cbb9b474e9e4d978008f361d3",
   clientSecret: "df155eab40c04a07b34a3e3db5e4caa2",
-  redirectUri: "https://personalised-spotify.herokuapp.com/login"
-  
+  redirectUri: "https://personalised-spotify.herokuapp.com/callback",
+  //http://localhost:9000/callback
 });
 
 const app = express();
@@ -30,59 +29,55 @@ const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json()); // recognize the incoming Request Object as a JSON Object.
-app.use("/recommendations", recommendationsRouter)
-app.use("/top-tracks", topTracksRouter)
-app.use("/top-artists", topArtistsRouter)
+app.use("/recommendations", recommendationsRouter);
+app.use("/top-tracks", topTracksRouter);
+app.use("/top-artists", topArtistsRouter);
 
-app.get("/callback", (req, res) => {
+app.get('/login', (req, res) => {
   res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
-
-app.get("/login", (req, res) => {
+app.get('/callback', (req, res) => {
   const error = req.query.error;
   const code = req.query.code;
+  const state = req.query.state;
 
   if (error) {
-    console.error("Callback Error:", error);
+    console.error('Callback Error:', error);
     res.send(`Callback Error: ${error}`);
     return;
   }
 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then((data) => {
-      const access_token = data.body["access_token"];
-      const refresh_token = data.body["refresh_token"];
-      const expires_in = data.body["expires_in"];
+    .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
 
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
 
-      console.log("access_token:", access_token);
-      console.log("refresh_token:", refresh_token);
+      console.log('access_token:', access_token);
+      console.log('refresh_token:', refresh_token);
 
       console.log(
         `Sucessfully retreived access token. Expires in ${expires_in} s.`
       );
-
-      app.get("/token", (req, res) => {
-        res.send({ access_token: access_token });
-      });
-
-      res.redirect("https://personalised-spotify.netlify.app/toptracks");
+      res.redirect('https://personalised-spotify.netlify.app/toptracks')
+      // http://localhost:3000/toptracks
 
       setInterval(async () => {
         const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body["access_token"];
+        const access_token = data.body['access_token'];
 
-        console.log("The access token has been refreshed!");
-        console.log("access_token:", access_token);
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
         spotifyApi.setAccessToken(access_token);
-      }, (expires_in / 1.25) * 1000);
+      }, expires_in / 2 * 1000);
     })
-    .catch((error) => {
-      console.error("Error getting Tokens:", error);
+    .catch(error => {
+      console.error('Error getting Tokens:', error);
       res.send(`Error getting Tokens: ${error}`);
     });
 });
@@ -99,73 +94,83 @@ app.get("/me", (req, res) => {
 });
 
 
-app.post('/create-playlist', (req, res) => {
-  const name = req.body.name
-  const list = req.body.track_id
- 
-  spotifyApi.createPlaylist(name, {'public': false })
-  .then(function(data) {
-    return data.body.id
-  }).then((response) => {
-    spotifyApi.addTracksToPlaylist(response, list)
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  })
-})
+app.post("/create-playlist", (req, res) => {
+  const name = req.body.name;
+  const list = req.body.track_id;
 
-
-
-
-app.post('/artist-top-tracks', (req, res) => {
-  const name = req.body.name
-  const artist_list = req.body.track_id
-  const uris = []
-
-  artist_list.forEach(artist => {
-  spotifyApi.getArtistTopTracks(artist, 'ES')
-  .then(function(data) {
-     const songs = data.body.tracks
-     songs.map(track => uris.push(track.uri))
-     console.log(uris)
-    }, function(err) {
-    console.log('Something went wrong!', err);
-  })
-})
-
-spotifyApi.createPlaylist(name, {'public': false })
-  .then(function(data) {
-    return data.body.id
-  }).then((response) => {
-    spotifyApi.addTracksToPlaylist(response, uris)
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  })
-  
-})
-
-app.post('/saved-tracks', (req, res) => {
-  const track_id = req.body.track_id
-  console.log(track_id)
-  spotifyApi.addToMySavedTracks([track_id])
-  .then(function(data) {
-    console.log('Added track!');
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
+  spotifyApi
+    .createPlaylist(name, { public: false })
+    .then(function (data) {
+      return data.body.id;
+    })
+    .then(
+      (response) => {
+        spotifyApi.addTracksToPlaylist(response, list);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
 });
 
-app.put('/saved-artist', (req, res) => {
-  const artist_id = req.body.track_id
-  console.log(artist_id)
-  spotifyApi.followArtists([artist_id])
-  .then(function(data) {
-    console.log('Added artist');
-  }, function(err) {
-    console.log('Something went wrong!', err);
+app.post("/artist-top-tracks", (req, res) => {
+  const name = req.body.name;
+  const artist_list = req.body.track_id;
+  const uris = [];
+
+  artist_list.forEach((artist) => {
+    spotifyApi.getArtistTopTracks(artist, "ES").then(
+      function (data) {
+        const songs = data.body.tracks;
+        songs.map((track) => uris.push(track.uri));
+        console.log(uris);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
   });
+
+  spotifyApi
+    .createPlaylist(name, { public: false })
+    .then(function (data) {
+      return data.body.id;
+    })
+    .then(
+      (response) => {
+        spotifyApi.addTracksToPlaylist(response, uris);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
 });
 
+app.post("/saved-tracks", (req, res) => {
+  const track_id = req.body.track_id;
+  console.log(track_id);
+  spotifyApi.addToMySavedTracks([track_id]).then(
+    function (data) {
+      console.log("Added track!");
+    },
+    function (err) {
+      console.log("Something went wrong!", err);
+    }
+  );
+});
 
+app.put("/saved-artist", (req, res) => {
+  const artist_id = req.body.track_id;
+  console.log(artist_id);
+  spotifyApi.followArtists([artist_id]).then(
+    function (data) {
+      console.log("Added artist");
+    },
+    function (err) {
+      console.log("Something went wrong!", err);
+    }
+  );
+});
 
 app.get("/recently-played", (req, res) => {
   spotifyApi
@@ -183,6 +188,5 @@ app.get("/recently-played", (req, res) => {
       }
     );
 });
-
 
 app.listen(PORT, () => console.log("HTTP Server up."));
